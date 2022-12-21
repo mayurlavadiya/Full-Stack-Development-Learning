8ef220df9b877858994f3d901bacb34299d3fc31
mayur lavadiya <mayurlavadiya123@gmail.com>
2022-12-21 20:05:08 +0530

    String

---
+++ Learn Javascript/05_string.js
@@ -0,0 +1,31 @@
+let singleQuotes = 'How are you';
+let doubleQuotes = "bole to christmas";
+
+// console.log(singleQuotes);
+// console.log(doubleQuotes);
+
+// char, charAtCode, substring
+// let char = singleQuotes.charAt(4); // 4 index pr je hse e print krse
+// let charcode = singleQuotes.charCodeAt(4); // ascii value  
+// let substr = singleQuotes.substring(2,8);
+
+// console.log(char);
+// console.log(substr);
+// console.log(charcode);
+
+// trim --- white space remove krva mate
+// let trimstring = singleQuotes.trim();
+// console.log(trimstring);
+
+// split & join
+let arrStr = singleQuotes.split("a"); // string ma jya i hse tya thi split krse
+console.log("split --- >", arrStr);
+let strjoin = arrStr.join("+");
+console.log(strjoin);
+
+
+
+
+
+
+
